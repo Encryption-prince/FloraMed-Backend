ad255d8edd131a155cf51a3c8aeb78ed00dbdf7a
Encryption-prince <subhammaity6827@gmail.com>
2025-05-09 19:36:13 +0530

    made the customoauth from live link to localhost

--- src/main/java/com/virtual/herbal/garden/Virtual_Herbs_Garden/config/SecurityConfig.java
+++ src/main/java/com/virtual/herbal/garden/Virtual_Herbs_Garden/config/SecurityConfig.java
@@ -44,7 +44,6 @@ public class SecurityConfig {
         return new JwtAuthenticationFilter(jwtUtil, userService);
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
